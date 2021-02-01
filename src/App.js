d179bd733cd73318432bc2d89736e6331fb4158d
smit <developers.comunev@gmail.com>
2021-02-01 13:36:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Register from "./components/RegisterComponent";
+import Login from './components/Login';
+import Greet from "./components/Greet";
+import OTP from "./components/OTP";
+import React from "react";
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                        <Route exact path="/" component={Greet} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/register" component={Register}/>
+                        <Route path='/verify-otp' component={OTP} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
